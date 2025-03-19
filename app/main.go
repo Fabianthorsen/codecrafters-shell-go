e5ea31412312ea91a510e4f604703223f1c08ea3
Fabian Thorsen <fabian.thorsen@statnett.no>
2025-03-19 13:26:38 +0100

    add exit 0

--- app/main.go
+++ app/main.go
@@ -17,6 +17,9 @@ func main() {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
+		if command == "exit 0\n" {
+			os.Exit(0)
+		}
 		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
 }
